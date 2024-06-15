1e6394b86a049716007f601a9dc1aaf8c3d9e823
sunilkrishna49 <sunilkrishna49@gmail.com>
2024-06-15 12:01:01 +0100

    completed with environmental_vairables

--- util/auth.js
+++ util/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_KEY = "AIzaSyBMtYRRaFIVC-9rLLMwMXPaKQ7kra7AaBY";
-const API_KEY = "AIzaSyDFuYx45hFXdLP5MyU2fnwYl97Q8fNiQc8";
+// const API_KEY = "";
+const API_KEY = "";
 
 const authenticate = async (mode, email, password) => {
   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
